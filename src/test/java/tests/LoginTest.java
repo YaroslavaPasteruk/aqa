2b009c0e2ae89294abdeece64b1de85f92603986
Yaroslava Pasteruk <pasteruk.jaroslava@gmail.com>
2019-07-03 20:43:11 +0300

    Add project

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void testUserIsAbleToLoginIntoApp() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "drivers"
+				+ File.separator + "chromedriver_win32" + File.separator + "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.cssSelector("input[value='LOGIN']")).click();
+	}
+
+}
